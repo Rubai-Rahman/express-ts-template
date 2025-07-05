23a4ff30a4cfc533a90b86dacdf4c83fce687f9e
Rubai Rahman <rubairahman1@gmail.com>
2025-07-05 11:29:37 +0600

    folder structure changing

--- src/config/environment.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
-export const PORT = process.env.PORT || 3000;
